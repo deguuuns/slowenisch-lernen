99a57bb384ec6c345b2f11a309bcc70f05a4ca5d
deguuuns <m9zzc4m5wy@privaterelay.appleid.com>
2026-08-24 04:28:44 +0200

    Phase 20: release lesson 8 in curriculum catalog

--- data/curriculum.ts
+++ data/curriculum.ts
@@ -1,6 +1,7 @@
 import { a1VocabularyExpansion } from '@/data/a1-vocabulary-expansion'
 import { lesson6, lesson6Conversation, lesson6Exercises, lesson6Sentences } from '@/data/lesson-6-shopping'
 import { lesson7, lesson7Conversation, lesson7Exercises, lesson7Sentences } from '@/data/lesson-7-travel'
+import { lesson8, lesson8Conversation, lesson8Exercises, lesson8Sentences } from '@/data/lesson-8-home-help'
 import {
   conversations as seedConversations,
   exercises as seedExercises,
@@ -10,10 +11,10 @@ import {
 } from '@/data/seed'
 
 export const vocabulary = [...seedVocabulary, ...a1VocabularyExpansion]
-export const lessons = [...seedLessons, lesson6, lesson7]
-export const sentences = [...seedSentences, ...lesson6Sentences, ...lesson7Sentences]
-export const exercises = [...seedExercises, ...lesson6Exercises, ...lesson7Exercises]
-export const conversations = [...seedConversations, lesson6Conversation, lesson7Conversation]
+export const lessons = [...seedLessons, lesson6, lesson7, lesson8]
+export const sentences = [...seedSentences, ...lesson6Sentences, ...lesson7Sentences, ...lesson8Sentences]
+export const exercises = [...seedExercises, ...lesson6Exercises, ...lesson7Exercises, ...lesson8Exercises]
+export const conversations = [...seedConversations, lesson6Conversation, lesson7Conversation, lesson8Conversation]
 
 export const releasedLessonIds = lessons.map(lesson => lesson.id)
 export const releasedVocabulary = vocabulary.filter(word => releasedLessonIds.includes(word.lesson))
