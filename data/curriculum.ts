4def23ddae5d6b1817a359be1a6a2a21d2cc10ad
deguuuns <m9zzc4m5wy@privaterelay.appleid.com>
2026-08-27 15:16:31 +0200

    Use canonical vocabulary catalog as the single released vocabulary source

--- data/curriculum.ts
+++ data/curriculum.ts
@@ -1,20 +1,19 @@
-import { a1VocabularyExpansion } from '@/data/a1-vocabulary-expansion'
-import { buildVocabularyV2 } from '@/data/a1-vocabulary-v2'
 import { lesson6, lesson6Conversation, lesson6Exercises, lesson6Sentences } from '@/data/lesson-6-shopping'
 import { lesson7, lesson7Conversation, lesson7Exercises, lesson7Sentences } from '@/data/lesson-7-travel'
 import { lesson8, lesson8Conversation, lesson8Exercises, lesson8Sentences } from '@/data/lesson-8-home-help'
+import { lesson1AtomicExercises, lesson1AtomicSentences } from '@/data/lesson-1-atomic'
 import {
   conversations as seedConversations,
   exercises as seedExercises,
   lessons as seedLessons,
   sentences as seedSentences,
-  vocabulary as seedVocabulary,
 } from '@/data/seed'
+import { vocabulary } from '@/data/vocabulary-catalog'
 
-export const vocabulary = buildVocabularyV2([...seedVocabulary, ...a1VocabularyExpansion])
+export { vocabulary }
 export const lessons = [...seedLessons, lesson6, lesson7, lesson8]
-export const sentences = [...seedSentences, ...lesson6Sentences, ...lesson7Sentences, ...lesson8Sentences]
-export const exercises = [...seedExercises, ...lesson6Exercises, ...lesson7Exercises, ...lesson8Exercises]
+export const sentences = [...seedSentences, ...lesson1AtomicSentences, ...lesson6Sentences, ...lesson7Sentences, ...lesson8Sentences]
+export const exercises = [...seedExercises, ...lesson1AtomicExercises, ...lesson6Exercises, ...lesson7Exercises, ...lesson8Exercises]
 export const conversations = [...seedConversations, lesson6Conversation, lesson7Conversation, lesson8Conversation]
 
 export const releasedLessonIds = lessons.map(lesson => lesson.id)
